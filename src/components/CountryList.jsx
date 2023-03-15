8d331369054d2bbce6cd8733ac9230b23f25b775
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-15 20:39:12 +0700

    create: CountryList components

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CountryList = () => {
+  return (
+    <section className="section pt-32">
+      <div className="container grid gap-8">
+        <div className="grid gap-12">
+          <div>Search</div>
+          <div>Filter</div>
+        </div>
+
+        <div className="grid gap-12 justify-self-center">
+          <div>Country card</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CountryList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 // import components
 import Header from "../components/Header";
+import CountryList from "../components/CountryList";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <main className="main">
+        <CountryList />
+      </main>
     </>
   );
 };
