4a40ab9e89ba2a812f8482a5d884cf802c03bdd0
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-16 22:29:57 +0700

    fixed: CountryCard & CountryDetails components

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 
 const CountryCard = ({ country }) => {
-  // destructure country
-  const { flags, name, population, region, capital } = country;
-
   return (
     <div className="w-max overflow-hidden rounded-lg shadow-sm transition-all duration-300 hover:-translate-y-1 hover:shadow-md">
       <div className="overflow-hidden">
         <img
-          src={flags.svg}
-          alt={name + "flag"}
+          src={country.flag}
+          alt={country.name + "flag"}
           className="h-[180px] w-[280px] object-cover object-center"
         />
       </div>
 
       <div className="max-w-[280px] overflow-hidden bg-white p-8">
-        <h3 className="section-title truncate pb-4 text-[18px]">{name}</h3>
+        <h3 className="section-title truncate pb-4 text-[18px]">
+          {country.name}
+        </h3>
 
         <ul className="flex flex-col gap-1 text-[14px]">
           {[
-            ["Population:", new Intl.NumberFormat().format(population)],
-            ["Region:", `${region}`],
-            ["Capital:", `${capital}`],
+            ["Population:", new Intl.NumberFormat().format(country.population)],
+            ["Region:", `${country.region}`],
+            ["Capital:", `${country.capital}`],
           ].map(([name, value]) => {
             return (
               <li key={name} className="flex items-center gap-1">
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -5,9 +5,6 @@ import { Link, useParams } from "react-router-dom";
 // import api
 import { getCountryByCode } from "../api/Api";
 
-// import image
-import Flag from "../assets/images/flag-be.svg";
-
 const CountryDetails = () => {
   const { code } = useParams();
   const [country, setCountry] = useState([]);
@@ -25,20 +22,6 @@ const CountryDetails = () => {
 
   console.log({ detail: country });
 
-  // destructure country
-  const {
-    flag,
-    name,
-    nativeName,
-    population,
-    region,
-    subregion,
-    capital,
-    topLevelDomain,
-    currencies,
-    languages,
-  } = country;
-
   return (
     <section className="section pt-32">
       <div className="container grid gap-10">
@@ -53,23 +36,28 @@ const CountryDetails = () => {
         <div className="grid gap-12 sm:mx-auto sm:max-w-2xl sm:justify-center lg:max-w-full lg:grid-cols-2 lg:items-center lg:gap-20">
           <div className="h-[270px] max-w-[580px] overflow-hidden rounded-md sm:h-[320px] lg:h-[430px] xl:min-w-[580px]">
             <img
-              src={flag}
-              alt={name + "flag"}
+              src={country.flag}
+              alt={country.name + "flag"}
               className="h-full w-full object-cover object-center"
             />
           </div>
 
           <div className="grid gap-6">
-            <h3 className="section-title text-[26px] lg:text-[32px]">{name}</h3>
+            <h3 className="section-title text-[26px] lg:text-[32px]">
+              {country.name}
+            </h3>
 
             <div className="grid gap-10 sm:grid-cols-2 sm:items-start lg:gap-20">
               <ul className="grid gap-3">
                 {[
-                  ["Native Name:", `${nativeName}`],
-                  ["Population:", new Intl.NumberFormat().format(population)],
-                  ["Region:", `${region}`],
-                  ["Sub Region:", `${subregion}`],
-                  ["Capital:", `${capital}`],
+                  ["Native Name:", `${country.nativeName}`],
+                  [
+                    "Population:",
+                    new Intl.NumberFormat().format(country.population),
+                  ],
+                  ["Region:", `${country.region}`],
+                  ["Sub Region:", `${country.subregion}`],
+                  ["Capital:", `${country.capital}`],
                 ].map(([name, value]) => {
                   return (
                     <li key={name} className="inline-flex items-start gap-1">
@@ -84,16 +72,22 @@ const CountryDetails = () => {
 
               <ul className="grid gap-3">
                 {[
-                  ["Top Level Domain:", `${topLevelDomain}`],
+                  ["Top Level Domain:", `${country.topLevelDomain}`],
                   [
                     "Currencies:",
-                    `${currencies ? currencies[0].name : "Unknown"}`,
+                    `${
+                      country.currencies
+                        ? country.currencies[0].name
+                        : "Unknown"
+                    }`,
                   ],
                   [
                     "Languages:",
                     `${
-                      languages
-                        ? languages.map((language) => language.name).join(", ")
+                      country.languages
+                        ? country.languages
+                            .map((language) => language.name)
+                            .join(", ")
                         : "Unknown"
                     }`,
                   ],
