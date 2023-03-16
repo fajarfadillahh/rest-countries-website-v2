c88399217ec45fa91e3c9630f4f622c43a3f2f9e
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-16 11:17:03 +0700

    create: Details page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import pages
 import Home from "./pages/Home";
+import Details from "./pages/Details";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/details/:code" element={<Details />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Details.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import components
+import Header from "../components/Header";
+
+const Details = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Details;
