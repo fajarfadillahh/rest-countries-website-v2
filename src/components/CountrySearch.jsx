5975a2a96056881234354f95feae38b12c658efe
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-19 21:18:13 +0700

    modified: dark mode CountrySearch components

--- src/components/CountrySearch.jsx
+++ src/components/CountrySearch.jsx
@@ -3,8 +3,8 @@ import { RiSearchLine } from "react-icons/ri";
 
 const CountrySearch = ({ setSearch }) => {
   return (
-    <form className="flex h-[56px] max-w-lg items-center rounded-md bg-white shadow-sm">
-      <div className="inline-flex h-full w-[80px] items-center justify-center text-gray-600">
+    <form className="flex h-[56px] max-w-lg items-center rounded-md bg-white shadow-sm dark:bg-gray-700">
+      <div className="inline-flex h-full w-[80px] items-center justify-center text-gray-600 dark:text-white">
         <RiSearchLine size="1.3rem" />
       </div>
 
@@ -12,7 +12,7 @@ const CountrySearch = ({ setSearch }) => {
         type="text"
         placeholder="Search for a country..."
         onChange={setSearch}
-        className="h-full w-full bg-transparent pr-8 font-semibold text-gray-900 outline-none placeholder:text-gray-600"
+        className="h-full w-full bg-transparent pr-8 font-semibold text-gray-900 outline-none placeholder:text-gray-600 dark:text-white dark:placeholder:text-white"
       />
     </form>
   );
