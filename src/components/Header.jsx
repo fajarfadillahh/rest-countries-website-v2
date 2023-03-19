7c60a8cc76909be800754d7907c5de3c9bcd7233
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-19 21:07:48 +0700

    modified: actived dark mode feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { RiMoonClearLine } from "react-icons/ri";
+import { RiMoonClearFill, RiMoonClearLine } from "react-icons/ri";
+
+// import custom hooks
+import useColorMode from "../hooks/UseColorMode";
 
 const Header = () => {
+  const [colorMode, setColorMode] = useColorMode();
+
   return (
     <header className="fixed inset-x-0 top-0 z-50 mx-auto max-w-[1440px] bg-white shadow-sm">
       <div className="container flex h-24 items-center justify-between">
@@ -10,8 +15,15 @@ const Header = () => {
           Where in the world?
         </Link>
 
-        <div className="inline-flex cursor-pointer items-center gap-[6px] font-semibold text-gray-900">
-          <RiMoonClearLine size="1.3rem" />
+        <div
+          className="inline-flex cursor-pointer items-center gap-[6px] font-semibold text-gray-900"
+          onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}
+        >
+          {colorMode === "light" ? (
+            <RiMoonClearLine size="1.3rem" />
+          ) : (
+            <RiMoonClearFill size="1.3rem" />
+          )}
           Dark Mode
         </div>
       </div>
