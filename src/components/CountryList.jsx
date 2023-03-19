acc693776c5eaa5e4b804c4ea1098afe76fd94de
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-19 22:03:53 +0700

    modified: added Loading animation on the CountryList

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
 
 // import api
 import { getAllCountries } from "../api/Api";
@@ -14,16 +15,20 @@ const CountryList = () => {
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [selectedRegion, setSelectedRegion] = useState("All");
   const [searchCountry, setSearchCountry] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getAllCountries()
-      .then((response) => {
-        setCountries(response);
-      })
-      .catch((error) => {
-        setCountries(null);
-        console.log(error);
-      });
+    setTimeout(() => {
+      getAllCountries()
+        .then((response) => {
+          setCountries(response);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setCountries(null);
+          console.log(error);
+        });
+    }, 3000);
   }, []);
 
   useEffect(() => {
@@ -56,25 +61,34 @@ const CountryList = () => {
 
   return (
     <section className="section pt-32">
-      <div className="container grid gap-8">
-        <div className="grid gap-12 lg:grid-cols-2">
-          <CountrySearch setSearch={handleSearchCountry} />
-          <CountryRegion setRegion={handleSelectedRegion} />
+      {loading ? (
+        <div className="fixed top-0 left-0 z-50 flex h-screen w-full flex-col items-center justify-center gap-4 bg-gray-900/90">
+          <BeatLoader color="#ffffff" size="1.3rem" />
+          <h3 className="text-[20px] font-semibold -tracking-tighter text-white">
+            Loading data...
+          </h3>
         </div>
+      ) : (
+        <div className="container grid gap-8">
+          <div className="grid gap-12 lg:grid-cols-2">
+            <CountrySearch setSearch={handleSearchCountry} />
+            <CountryRegion setRegion={handleSelectedRegion} />
+          </div>
 
-        <div className="grid gap-12 justify-self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {filteredCountries?.map((country) => {
-            return (
-              <Link
-                to={`/details/${country.alpha3Code.toLowerCase()}`}
-                key={country.numericCode}
-              >
-                <CountryCard country={country} />
-              </Link>
-            );
-          })}
+          <div className="grid gap-12 justify-self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {filteredCountries?.map((country) => {
+              return (
+                <Link
+                  to={`/details/${country.alpha3Code.toLowerCase()}`}
+                  key={country.numericCode}
+                >
+                  <CountryCard country={country} />
+                </Link>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
